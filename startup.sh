b14499f2433bd74485ab275c137cb47b1a091ce0
Vitaly Pronkin <pronvit@me.com>
2017-02-09 17:00:34 +1300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 fallocate -l 4G /swp ; chmod 600 /swp ; mkswap /swp ; swapon /swp ; echo '/swp none swap sw 0 0' >>/etc/fstab
 apt install unzip
-wget https://gist.github.com/pronvit/af21f8218d98fcb31c01917da2da076e/raw/28b548210df49550e5a7f171804eb5053570c42f/benchmark.lua
+wget https://github.com/mifki/dfremote-cloud-benchmark/raw/master/benchmark.lua
 wget http://mifki.com/a/t/gloveloved.zip
 unzip gloveloved.zip -d save/
 docker run --name=df -dt -v $PWD/save:/df_linux/data/save -v $PWD/benchmark.lua:/df_linux/hack/scripts/benchmark.lua mifki/dfremote
